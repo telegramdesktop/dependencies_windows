0dafdc231be692dc9b638e38ccebb8c80a3e8f14
Christoph Auer <auer.christoph@outlook.at>
2017-02-17 16:32:44 +0100

    More cleanup

--- cleanup.bat
+++ cleanup.bat
@@ -20,18 +20,19 @@ GOTO:EOF
 
 :deleteSourceFiles
     call:logInfo "Deleting source files"
-	
-	PowerShell Remove-Item .\*\* -include *.cpp -force -recurse
-	PowerShell Remove-Item .\*\* -include *.c   -force -recurse
-	PowerShell Remove-Item .\*\* -include *.cc  -force -recurse
-	PowerShell Remove-Item .\*\* -include *.asm -force -recurse
-	PowerShell Remove-Item .\*\* -include *.S   -force -recurse
-	PowerShell Remove-Item .\*\* -include *.v   -force -recurse
+
+	PowerShell Remove-Item .\*\* -include *.cpp  -force -recurse
+	PowerShell Remove-Item .\*\* -include *.c    -force -recurse
+	PowerShell Remove-Item .\*\* -include *.cc   -force -recurse
+	PowerShell Remove-Item .\*\* -include *.asm  -force -recurse
+	PowerShell Remove-Item .\*\* -include *.S    -force -recurse
+	PowerShell Remove-Item .\*\* -include *.v    -force -recurse
+	PowerShell Remove-Item .\*\* -include *.java -force -recurse
 GOTO:EOF
 
 :deleteProjectFiles
     call:logInfo "Deleting solution and project files"
-	
+
 	PowerShell Remove-Item .\*\* -include *.mk      -force -recurse
 	PowerShell Remove-Item .\*\* -include *.sln     -force -recurse
 	PowerShell Remove-Item .\*\* -include *.vcproj  -force -recurse
@@ -42,27 +43,28 @@ GOTO:EOF
 
 :deleteScripts
     call:logInfo "Deleting scripts"
-	
+
 	::keep ninja/ninja.exe
 	rename .\ninja\ninja.exe ninja.KEEP_ME
 	PowerShell Remove-Item .\*\* -include *.exe -force -recurse
 	rename .\ninja\ninja.KEEP_ME ninja.exe
-	
+
 	PowerShell Remove-Item .\*\* -include *.sh  -force -recurse
 	PowerShell Remove-Item .\*\* -include *.ps1 -force -recurse
 	PowerShell Remove-Item .\*\* -include *.bat -force -recurse
+	PowerShell Remove-Item .\*\* -include *.dll -force -recurse
 GOTO:EOF
 
 :deleteLogFiles
     call:logInfo "Deleting log files"
-	
+
 	PowerShell Remove-Item .\*\* -include *.log  -force -recurse
 	PowerShell Remove-Item .\*\* -include *.tlog -force -recurse
 GOTO:EOF
 
 :deleteImages
     call:logInfo "Deleting images"
-	
+
 	PowerShell Remove-Item .\*\* -include *.jpg  -force -recurse
 	PowerShell Remove-Item .\*\* -include *.jpeg -force -recurse
 	PowerShell Remove-Item .\*\* -include *.png  -force -recurse
@@ -70,7 +72,7 @@ GOTO:EOF
 
 :deleteDocumentation
     call:logInfo "Deleting documentation"
-	
+
 	PowerShell Remove-Item .\*\* -include *.md  -force -recurse
 	PowerShell Remove-Item .\*\* -include *.qdoc  -force -recurse
 	PowerShell Remove-Item .\*\* -include *.txt  -force -recurse
@@ -78,9 +80,11 @@ GOTO:EOF
 
 :deleteOther
     call:logInfo "Deleting other files"
-	
+
 	PowerShell Remove-Item .\*\* -include *.js        -force -recurse
 	PowerShell Remove-Item .\*\* -include *.css       -force -recurse
 	PowerShell Remove-Item .\*\* -include *.xsl       -force -recurse
+	PowerShell Remove-Item .\*\* -include *.mo        -force -recurse
+	PowerShell Remove-Item .\*\* -include *.m4        -force -recurse
 	PowerShell Remove-Item .\*\* -include *.gitignore -force -recurse
 GOTO:EOF
